7795ce6565457ea172d1317451c44f786aa62ab7
github username <github email address>
2018-12-17 20:28:39 -0500

    path issue fixed, hello world showing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './', 'index.html')));
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './public')}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
