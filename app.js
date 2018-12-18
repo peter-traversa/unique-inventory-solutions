6b99deefee980f5c4d70997647d2db8d9377f801
Peter Traversa <peter.traversa@gmail.com>
2018-12-17 21:09:59 -0500

    Html hello world (#5)
    * html file added, express server changed
    
    * path issue fixed, hello world showing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './public')}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
