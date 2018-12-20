5f677bdff9822131319b17c08bcca50724ee776f
github username <github email address>
2018-12-19 19:19:51 -0500

    webpack and express configurations built, build folder gitignored

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './public')}));
+app.use(express.static('build'));
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './build')}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
