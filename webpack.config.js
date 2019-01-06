81293c65a499a287ac40d3e0cbf90d73f935f94b
github username <github email address>
2019-01-06 14:15:31 -0500

    not working yet

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname + '/build',
-    filename: 'index_bundle.js'
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(
+      {template: path.join(__dirname,'/src/index.html')}
+    )
   ]
 };
