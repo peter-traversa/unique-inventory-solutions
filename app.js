33577bf793ee1c296473426d696315e0da2a9af8
Pete Traversa <peter.traversa@gmail.com>
2019-03-05 16:37:36 -0500

    .env added to gitignore

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const webpackConfig = require('./webpack.config.babel.js');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 app.use(webpackMiddleware(webpack(webpackConfig)));
 app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './build')}));
 app.listen(port, () => console.log(`Listening on port ${port}...`));
