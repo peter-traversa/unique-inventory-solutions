67b3794785fc7d40deabd2f6d65447606a305456
Pete Traversa <peter.traversa@gmail.com>
2019-03-05 17:11:32 -0500

    port defined in .env, working

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.babel.js');
 const app = express();
-const port = 3000;
-
-require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.use(webpackMiddleware(webpack(webpackConfig)));
 app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './build')}));
