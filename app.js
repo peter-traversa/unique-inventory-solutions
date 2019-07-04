cd4de740e235a145d8ffe336fb9fe33390e9af8d
andigan <andrew.nease.code@gmail.com>
2019-07-03 22:27:33 -0400

    webpack build instead of using webpack middle; use custom-env

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.babel.js');
+const path = require('path');
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('./webpack.config.babel.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
-app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './build')}));
+// app.use(webpackMiddleware(webpack(webpackConfig)));
+app.use(express.static('dist'));
+
+
+app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './dist')}));
 app.listen(port, () => console.log(`Listening on port ${port}...`));
